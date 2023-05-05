57d8db2127108e7f77476347e0bae3370e2831d4
Patrycja Wysocka <w512patrycja@gmail.com>
2023-05-05 12:29:04 +0200

    fixed linter warnings

--- client.py
+++ client.py
@@ -4,12 +4,13 @@ import PySimpleGUI as sg
 from enum import Enum
 import argparse
 
-class client :
+
+class client:
 
     # ******************** TYPES *********************
     # *
     # * @brief Return codes for the protocol methods
-    class RC(Enum) :
+    class RC(Enum):
         OK = 0
         ERROR = 1
         USER_ERROR = 2
@@ -30,7 +31,7 @@ class client :
     # * @return USER_ERROR if the user is already registered
     # * @return ERROR if another error occurred
     @staticmethod
-    def  register(user, window):
+    def register(user, window):
         window['_SERVER_'].print("s> REGISTER OK")
         #  Write your code here
         return client.RC.ERROR
@@ -42,11 +43,10 @@ class client :
     # 	 * @return USER_ERROR if the user does not exist
     # 	 * @return ERROR if another error occurred
     @staticmethod
-    def  unregister(user, window):
+    def unregister(user, window):
         window['_SERVER_'].print("s> UNREGISTER OK")
         #  Write your code here
         return client.RC.ERROR
-
 
     # *
     # * @param user - User name to connect to the system
@@ -54,12 +54,12 @@ class client :
     # * @return OK if successful
     # * @return USER_ERROR if the user does not exist or if it is already connected
     # * @return ERROR if another error occurred
+
     @staticmethod
-    def  connect(user, window):
+    def connect(user, window):
         window['_SERVER_'].print("s> CONNECT OK")
         #  Write your code here
         return client.RC.ERROR
-
 
     # *
     # * @param user - User name to disconnect from the system
@@ -67,8 +67,9 @@ class client :
     # * @return OK if successful
     # * @return USER_ERROR if the user does not exist
     # * @return ERROR if another error occurred
+
     @staticmethod
-    def  disconnect(user, window):
+    def disconnect(user, window):
         window['_SERVER_'].print("s> DISCONNECT OK")
         #  Write your code here
         return client.RC.ERROR
@@ -81,7 +82,7 @@ class client :
     # * @return USER_ERROR if the user is not connected (the message is queued for delivery)
     # * @return ERROR the user does not exist or another error occurred
     @staticmethod
-    def  send(user, message, window):
+    def send(user, message, window):
         window['_SERVER_'].print("s> SEND MESSAGE OK")
         print("SEND " + user + " " + message)
         #  Write your code here
@@ -97,27 +98,26 @@ class client :
     # * @return USER_ERROR if the user is not connected (the message is queued for delivery)
     # * @return ERROR the user does not exist or another error occurred
     @staticmethod
-    def  sendAttach(user, message, file, window):
+    def sendAttach(user, message, file, window):
         window['_SERVER_'].print("s> SENDATTACH MESSAGE OK")
         print("SEND ATTACH " + user + " " + message + " " + file)
         #  Write your code here
         return client.RC.ERROR
 
     @staticmethod
-    def  connectedUsers(window):
+    def connectedUsers(window):
         window['_SERVER_'].print("s> CONNECTED USERS OK")
         #  Write your code here
         return client.RC.ERROR
 
-
     @staticmethod
     def window_register():
-        layout_register = [[sg.Text('Ful Name:'),sg.Input('Text',key='_REGISTERNAME_', do_not_clear=True, expand_x=True)],
-                            [sg.Text('Alias:'),sg.Input('Text',key='_REGISTERALIAS_', do_not_clear=True, expand_x=True)],
-                            [sg.Text('Date of birth:'),sg.Input('',key='_REGISTERDATE_', do_not_clear=True, expand_x=True, disabled=True, use_readonly_for_disable=False),
-                            sg.CalendarButton("Select Date",close_when_date_chosen=True, target="_REGISTERDATE_", format='%d-%m-%Y',size=(10,1))],
-                            [sg.Button('SUBMIT', button_color=('white', 'blue'))]
-                            ]
+        layout_register = [[sg.Text('Ful Name:'), sg.Input('Text', key='_REGISTERNAME_', do_not_clear=True, expand_x=True)],
+                           [sg.Text('Alias:'), sg.Input('Text', key='_REGISTERALIAS_', do_not_clear=True, expand_x=True)],
+                           [sg.Text('Date of birth:'), sg.Input('', key='_REGISTERDATE_', do_not_clear=True, expand_x=True, disabled=True, use_readonly_for_disable=False),
+                            sg.CalendarButton("Select Date", close_when_date_chosen=True, target="_REGISTERDATE_", format='%d-%m-%Y', size=(10, 1))],
+                           [sg.Button('SUBMIT', button_color=('white', 'blue'))]
+                           ]
 
         layout = [[sg.Column(layout_register, element_justification='center', expand_x=True, expand_y=True)]]
 
@@ -141,18 +141,18 @@ class client :
                 break
         window.Close()
 
-
     # *
     # * @brief Prints program usage
-    @staticmethod
-    def usage() :
-        print("Usage: python3 py -s <server> -p <port>")
 
+    @staticmethod
+    def usage():
+        print("Usage: python3 py -s <server> -p <port>")
 
     # *
     # * @brief Parses program execution arguments
+
     @staticmethod
-    def  parseArguments(argv) :
+    def parseArguments(argv):
         parser = argparse.ArgumentParser()
         parser.add_argument('-s', type=str, required=True, help='Server IP')
         parser.add_argument('-p', type=int, required=True, help='Server Port')
@@ -163,14 +163,13 @@ class client :
             return False
 
         if ((args.p < 1024) or (args.p > 65535)):
-            parser.error("Error: Port must be in the range 1024 <= port <= 65535");
-            return False;
+            parser.error("Error: Port must be in the range 1024 <= port <= 65535")
+            return False
 
         client._server = args.s
         client._port = args.p
 
         return True
-
 
     def main(argv):
 
@@ -178,27 +177,24 @@ class client :
             client.usage()
             exit()
 
-        lay_col = [[sg.Button('REGISTER',expand_x=True, expand_y=True),
-                sg.Button('UNREGISTER',expand_x=True, expand_y=True),
-                sg.Button('CONNECT',expand_x=True, expand_y=True),
-                sg.Button('DISCONNECT',expand_x=True, expand_y=True),
-                sg.Button('CONNECTED USERS',expand_x=True, expand_y=True)],
-                [sg.Text('Dest:'),sg.Input('User',key='_INDEST_', do_not_clear=True, expand_x=True),
-                sg.Text('Message:'),sg.Input('Text',key='_IN_', do_not_clear=True, expand_x=True),
-                sg.Button('SEND',expand_x=True, expand_y=False)],
-                [sg.Text('Attached File:'), sg.In(key='_FILE_', do_not_clear=True, expand_x=True), sg.FileBrowse(),
-                sg.Button('SENDATTACH',expand_x=True, expand_y=False)],
-                [sg.Multiline(key='_CLIENT_', disabled=True, autoscroll=True, size=(60,15), expand_x=True, expand_y=True),
-                sg.Multiline(key='_SERVER_', disabled=True, autoscroll=True, size=(60,15), expand_x=True, expand_y=True)],
-                [sg.Button('QUIT', button_color=('white', 'red'))]
-            ]
-
+        lay_col = [[sg.Button('REGISTER', expand_x=True, expand_y=True),
+                    sg.Button('UNREGISTER', expand_x=True, expand_y=True),
+                    sg.Button('CONNECT', expand_x=True, expand_y=True),
+                    sg.Button('DISCONNECT', expand_x=True, expand_y=True),
+                    sg.Button('CONNECTED USERS', expand_x=True, expand_y=True)],
+                   [sg.Text('Dest:'), sg.Input('User', key='_INDEST_', do_not_clear=True, expand_x=True),
+                    sg.Text('Message:'), sg.Input('Text',key='_IN_', do_not_clear=True, expand_x=True),
+                    sg.Button('SEND', expand_x=True, expand_y=False)],
+                   [sg.Text('Attached File:'), sg.In(key='_FILE_', do_not_clear=True, expand_x=True), sg.FileBrowse(),
+                    sg.Button('SENDATTACH', expand_x=True, expand_y=False)],
+                   [sg.Multiline(key='_CLIENT_', disabled=True, autoscroll=True, size=(60, 15), expand_x=True, expand_y=True),
+                    sg.Multiline(key='_SERVER_', disabled=True, autoscroll=True, size=(60, 15), expand_x=True, expand_y=True)],
+                   [sg.Button('QUIT', button_color=('white', 'red'))]]
 
         layout = [[sg.Column(lay_col, element_justification='center', expand_x=True, expand_y=True)]]
 
-        window = sg.Window('Messenger', layout, resizable=True, finalize=True, size=(1000,400))
+        window = sg.Window('Messenger', layout, resizable=True, finalize=True, size=(1000, 400))
         window.bind("<Escape>", "-ESCAPE-")
-
 
         while True:
             event, values = window.Read()
@@ -207,18 +203,17 @@ class client :
                 sg.Popup('Closing Client APP', title='Closing', button_type=5, auto_close=True, auto_close_duration=1)
                 break
 
-            #if (values['_IN_'] == '') and (event != 'REGISTER' and event != 'CONNECTED USERS'):
-             #   window['_CLIENT_'].print("c> No text inserted")
-             #   continue
+            # if (values['_IN_'] == '') and (event != 'REGISTER' and event != 'CONNECTED USERS'):
+            #   window['_CLIENT_'].print("c> No text inserted")
+            #   continue
 
-            if (client._alias == None or client._username == None or client._alias == 'Text' or client._username == 'Text' or client._date == None) and (event != 'REGISTER'):
+            if (client._alias is None or client._username is None or client._alias == 'Text' or client._username == 'Text' or client._date is None) and (event != 'REGISTER'):
                 sg.Popup('NOT REGISTERED', title='ERROR', button_type=5, auto_close=True, auto_close_duration=1)
                 continue
 
             if (event == 'REGISTER'):
                 client.window_register()
-
-                if (client._alias == None or client._username == None or client._alias == 'Text' or client._username == 'Text' or client._date == None):
+                if (client._alias is None or client._username is None or client._alias == 'Text' or client._username == 'Text' or client._date is None):
                     sg.Popup('NOT REGISTERED', title='ERROR', button_type=5, auto_close=True, auto_close_duration=1)
                     continue
 
@@ -229,25 +224,21 @@ class client :
                 window['_CLIENT_'].print('c> UNREGISTER ' + client._alias)
                 client.unregister(client._alias, window)
 
-
             elif (event == 'CONNECT'):
                 window['_CLIENT_'].print('c> CONNECT ' + client._alias)
                 client.connect(client._alias, window)
 
-
             elif (event == 'DISCONNECT'):
                 window['_CLIENT_'].print('c> DISCONNECT ' + client._alias)
                 client.disconnect(client._alias, window)
-
 
             elif (event == 'SEND'):
                 window['_CLIENT_'].print('c> SEND ' + values['_INDEST_'] + " " + values['_IN_'])
 
                 if (values['_INDEST_'] != '' and values['_IN_'] != '' and values['_INDEST_'] != 'User' and values['_IN_'] != 'Text') :
                     client.send(values['_INDEST_'], values['_IN_'], window)
-                else :
+                else:
                     window['_CLIENT_'].print("Syntax error. Insert <destUser> <message>")
-
 
             elif (event == 'SENDATTACH'):
 
@@ -255,15 +246,12 @@ class client :
 
                 if (values['_INDEST_'] != '' and values['_IN_'] != '' and values['_FILE_'] != '') :
                     client.sendAttach(values['_INDEST_'], values['_IN_'], values['_FILE_'], window)
-                else :
+                else:
                     window['_CLIENT_'].print("Syntax error. Insert <destUser> <message> <attachedFile>")
-
 
             elif (event == 'CONNECTED USERS'):
                 window['_CLIENT_'].print("c> CONNECTEDUSERS")
                 client.connectedUsers(window)
-
-
 
             window.Refresh()
 
